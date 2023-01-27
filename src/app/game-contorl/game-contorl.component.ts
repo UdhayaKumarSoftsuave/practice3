9a1eb88e323556916fb7efd23c42f7d7f2468dfd
kumarudhaya <kumaudhayagit@gmail.com>
2023-01-27 10:12:09 +0530

    First solution

--- src/app/game-contorl/game-contorl.component.ts
+++ src/app/game-contorl/game-contorl.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-game-contorl',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./game-contorl.component.css']
 })
 export class GameContorlComponent {
+
+  refer : any ;
+  count : number = 0;
+  counts : number[] = [];
+
+  onStart(){
+    this.refer = setInterval(() => {
+      this.count = this.count + 1;
+      this.counts.push(this.count);
+      console.log(this.counts);
+      
+    }, 1000);
+  }
+  onEnd(){
+    clearInterval(this.refer);
+  }
 
 }
